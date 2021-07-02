410bec6e6d91c297cc42f087fc6662d2c2c66995
Mahier Sydow <mahier.sydow8@gmail.com>
2021-07-02 22:23:37 +0900

    all pages design/functionality/responsiveness

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './contact.scss';
 
 function Contact() {
+    const[message, setMessage] = useState(false)
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); 
+        setMessage(true);
+    }
+
     return (
         <div className="contact" id="contact">
-            scscsc
+            <div className="left">
+                <img src="assets/shake.svg" alt="" />
+            </div>
+            <div className="right">
+                <h2>Contact.</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Email" />
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">Send</button>
+                    {message && <span>Thanks, I'll reply ASAP :)</span>}
+                </form>
+            </div>
         </div>
     )
 }
--- src/components/portfolioList/PortfolioList.jsx
+++ src/components/portfolioList/PortfolioList.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import "./portfolioList.scss";
 
-import './portfolioList.scss';
-
-function PortfolioList() {
-    return (
-        <div className="portfolioList">
-            
-        </div>
-    )
+ function PortfolioList({ id, title, active, setSelected }) {
+  return (
+    <li
+      className={active ? "portfolioList active" : "portfolioList"}
+      onClick={() => setSelected(id)}
+    >
+      {title}
+    </li>
+  );
 }
 
 export default PortfolioList;
